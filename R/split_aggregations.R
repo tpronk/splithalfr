6b036b8aa9c1be3d54ab8f9591eb28d8c936ca94
tpronk <pronkthomas@gmail.com>
2025-04-28 23:52:31 +0200

    refactor: replace bcaboot by boot package

--- R/split_aggregations.R
+++ R/split_aggregations.R
@@ -70,32 +70,42 @@ not_missing_casewise <- function (ds) {
   ))
 }
 
-#' Calculate nonparametric bias-corrected and accelerated bootstrap confidence
-#' intervals for coefficients averaged across split replications
+#' Generate bootstrap replicates of a coefficient split-half reliability
+#' estimate by sampling participants
 #'
-#' Calculates nonparametric bias-corrected and accelerated bootstrap confidence intervals
-#' via \code{\link[bcaboot]{bcajack}}. Coefficients are \code{ds} should be a data frame as returned by
-#' \code{\link{by_split}}: Each unique value of the column \code{participant} is considered a independent
-#' sample of the target population. For each unique value of the column \code{split} in
-#' \code{ds}, it selects the corresponding rows in \code{ds}, and passes the
-#' values in the columns \code{score_1} and \code{score_2} as the first and
-#' second argument to \code{fn_coef}. Any row in \code{ds} for which
+#' Generates bootstrap replicates via \code{\link[boot]{boot}}. The parameter
+#' \code{ds} should be a data frame as returned by \code{\link{by_split}}: Each
+#' unique value of the column \code{participant} is considered a independent
+#' sample of the target population. For each unique value of the column
+#' \code{split} in \code{ds}, it selects the corresponding rows in \code{ds},
+#' and passes the values in the columns \code{score_1} and \code{score_2} as the
+#' first and second argument to \code{fn_coef}. Any row in \code{ds} for which
 #' \code{score_1} or \code{score_2} is NA is pairwise removed before passing the
 #' data to \code{fn_coef}. Any coefficient that is NA is removed before passing
-#' the data to to \code{fn_summary}.
+#' the data to \code{fn_summary}.
 #' 
-#' For averaging internal consistency coefficients, see Feldt and Charter (2006). For more 
-#' information about bias-corrected and accelerated bootstrap confidence intervals, see
-#' Efron (1987).
+#' For a practical example, see one of the vignettes for getting started with
+#' the splithalfr. Also, note that the boot package supports parallel processing
+#' via the parameters `parallel` and `ncpus`.
+#'
+#' For averaging internal consistency coefficients, see Feldt and Charter
+#' (2006). For more information about bias-corrected and accelerated bootstrap
+#' confidence intervals, see Efron (1987).
 #'
 #' @param ds (data frame) a data frame with columns \code{split},
 #'   \code{score_1}, and \code{score_2}
-#' @param fn_coef (function) a function that calculates a bivariate coefficient.
-#' @param fn_average (function) a function that calculates an average across coefficients 
-#' @param replications (integer) number of bootstrap replications
-#' @param ... Additional arguments passed to \code{\link[bcaboot]{bcajack}}
+#' @param fn_coef (function) a function that calculates a bivariate
+#'   (reliability) coefficient
+#' @param fn_average (function) a function that calculates an average across
+#'   coefficients
+#' @param bootstrap_replications (integer) number of bootstrap replications
+#' @param parallel (character) Type of parallel processing (if any) used for bootstrapping. See \code{\link[boot]{boot}}
+#' @param ncpus (character) Number of cores for parallel processing. See \code{\link[boot]{boot}}
+#' @param ... Additional arguments passed to \code{\link[boot]{boot}}
 #' @return Confidence interval
 #' @examples
+#' # Import boot library
+#' library(boot)
 #' # Generate five splits with scores that are correlated 0.00, 0.25, 0.5, 0.75, and 1.00
 #' library(MASS)
 #' ds_splits = data.frame(V1 = numeric(), V2 = numeric(), split = numeric())
@@ -104,24 +114,37 @@ not_missing_casewise <- function (ds) {
 #'   ds_splits = rbind(ds_splits, cbind(vars, r, 1 : 10))
 #' }
 #' names(ds_splits) = c("score_1", "score_2", "replication", "participant")
-#' # Calculate confidence interval
-#' split_ci(ds_splits, cor)
+#' # Conduct bootstrap
+#' bootstrap_result <- split_ci(ds_splits, cor, mean)
+#' # Get boosted and accelerated confidence intervals
+#' print(boot.ci(bootstrap_result, type="bca"))
 #' @section References:
 #'
 #'  Efron, B. (1987). Better bootstrap confidence intervals. \emph{Journal of the
 #'  American statistical Association}, 82(397), 171-185.
-#'  \doi{10.1080/01621459.1987.10478410}
+#'   \doi{10.1080/01621459.1987.10478410}
 #'
-#'  Feldt, L. S., & Charter, R. A. (2006). Averaging internal consistency
-#'  reliability coefficients. \emph{Educational and Psychological Measurement},
-#'  66(2), 215-227.
-#'  \doi{10.1177/0013164404273947}
+#'   Feldt, L. S., & Charter, R. A. (2006). Averaging internal consistency
+#'   reliability coefficients. \emph{Educational and Psychological Measurement},
+#'   66(2), 215-227. \doi{10.1177/0013164404273947}
 #' @family split aggregation functions
 #' @export
-split_ci <- function(ds, fn_coef, fn_average = function (values) { mean(values, na.rm = TRUE) }, replications = 1000, ...) {
+split_ci <- function(ds, fn_coef, fn_average, bootstrap_replications = 1000, parallel = "snow", ncpus = detectCores(), ...) {
+  # Replicated in split_ci to let it be imported into a snow cluster constructed by boot
+  not_missing_casewise <- function (ds) {
+    return (apply(
+      ds,
+      1,
+      function (ds_row) {
+        return (all(!is.na(ds_row)))
+      }
+    ))
+  }
+  
   # Split iterations to average
   split_indexes = unique(ds$replication)
-  # Prepare data for bcajack
+  
+  # Prepare data for bootstrap; one row per participant
   ds_wide <- reshape(
     ds,
     timevar = c("replication"),
@@ -129,8 +152,10 @@ split_ci <- function(ds, fn_coef, fn_average = function (values) { mean(values, 
     idvar   = c("participant"),
     direction = "wide"
   )
+  
   # Calculates averaged coefficient per split
-  func <- function (x) {
+  func <- function (original, indexes) {
+    ds_resampled <- original[indexes,]
     coefs <- sapply(
       split_indexes,
       function (split_i, ds_wide) {
@@ -143,17 +168,12 @@ split_ci <- function(ds, fn_coef, fn_average = function (values) { mean(values, 
           ds[, paste("score_2", split_i, sep = ".")]
         )
       },
-      ds_wide = x
+      ds_wide = ds_resampled
     )  
     return (fn_average(coefs))
   }
-  # Calculate bias-corrected and accelerated bootstrap confidence limits
-  #library(bcaboot)
-  return(bcajack(
-    x = ds_wide,
-    B = replications,
-    func = func,
-    alpha = 0.025,
-    ...
-  ))
+  
+  # Conduct and return bootstrap
+  boot_result = boot(data = ds_wide, statistic = func, R = bootstrap_replications, ...)
+  return (boot_result)  
 }
